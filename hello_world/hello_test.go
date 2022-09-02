cc6ca7ab663752e73bd9456469e225ed650ae973
gabrielluizsf <gabrielluizads123@gmail.com>
2022-09-01 23:33:55 -0300

    go test init

---
+++ hello_world/hello_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestHello(test *testing.T){ 
+  checkExpectedResult := func(test *testing.T,result,expected string){
+	test.Helper();
+	if result != expected{
+		test.Errorf("\nResult:%s\nExpected:%s",result,expected);
+	}
+  }
+
+  test.Run("Says Hello To a Person"func(test *testing.T){
+	result := Hello("Yourname")
+	expected := "Hello Yourname"
+	checkExpectedResult(test,result,expected)
+  })
+  
+
+}
